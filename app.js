9a7fbca88cacdbe69f3b7fc6d14c5bbbf29e458f
Gentuar Lushtaku <93317180+itslushtaK@users.noreply.github.com>
2024-11-04 07:45:28 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,9 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "https://kerko-gjej.vercel.app/", // Allow requests from your frontend
+    origin: "https://kerko-gjej.vercel.app/",
+          "https://kerko-gjej-q40wtwxqn-gentuar-lushtakus-projects.vercel.app",
+// Allow requests from your frontend
   })
 );
 
