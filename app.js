f0c4ea37e38e86123ed7220f6ade1c44a2ff90c6
Gentuar Lushtaku <93317180+itslushtaK@users.noreply.github.com>
2024-11-04 23:59:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "kerko-gjej-production.up.railway.app", // Allow requests from your frontend
+    origin: "https://kerko-gjej-production.up.railway.app", // Allow requests from your frontend
   })
 );
 
