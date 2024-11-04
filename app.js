49d9231117e21793bca8a7cec94ac273653c298a
Gentuar Lushtaku <93317180+itslushtaK@users.noreply.github.com>
2024-11-04 07:37:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000", // Allow requests from your frontend
+    origin: "https://kerko-gjej.vercel.app/", // Allow requests from your frontend
   })
 );
 
